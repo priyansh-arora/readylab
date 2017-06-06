af3e22e50be43ee9647c97ab515b9cefd138ce6d
MSREADYDEMO <praro@microsoft.com>
2017-06-06 13:50:17 +0530

    Add files via upload

---
+++ ARMTemplates/swap.ps1
@@ -0,0 +1,16 @@
+﻿Param(
+	[string] $Name,
+	[string] $FromSlot="staging"
+)
+
+if(!(Get-Module Azure) -or !(Get-Module AzureRM))
+{
+	Import-Module Azure
+	Import-Module AzureRM
+}
+
+#Swap backend first and then frontend
+Switch-AzureWebsiteSlot -Name "${Name}Api" -Slot1 $FromSlot -Slot2 production -Force -Verbose
+Switch-AzureWebsiteSlot -Name $Name -Slot1 $FromSlot -Slot2 production -Force -Verbose
+
+Write-Host "Done"
